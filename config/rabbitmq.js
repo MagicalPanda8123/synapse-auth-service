3f218cac21a846eced2a03a1b33049b99e0446d4
Khang Nguyen <m.khang8123@gmail.com>
2025-09-20 14:03:51 +0700

    fix(rabbitmq): fix connection retry logic

--- config/rabbitmq.js
+++ config/rabbitmq.js
@@ -4,37 +4,34 @@ let channel, connection
 let shuttingDown = false
 
 export async function getChannel() {
-  if (channel) return channel
-
-  try {
-    connection = await amqp.connect(
-      process.env.RABBITMQ_URL ||
-        'amqp://localhostamqp://guest:guest@localhost:5672'
-    )
-
-    connection.on('error', (err) => {
-      console.error('[RabbitMQ] Connection error: ', err.message)
-    })
-
-    connection.on('close', () => {
-      channel = null
-      // Only reconnect on unexpected disconnects
-      if (!shuttingDown) {
+  while (!channel) {
+    try {
+      connection = await amqp.connect(
+        process.env.RABBITMQ_URL || 'amqp://localhost'
+      )
+      connection.on('error', (err) => {
+        console.error('[RabbitMQ] Connection error: ', err.message)
+      })
+      connection.on('close', () => {
         console.warn('[RabbitMQ] Connection closed. Reconnecting...')
-        setTimeout(getChannel, 10000) // reconnect after 10s
-      } else {
-        shuttingDown = false
-      }
-    })
-
-    channel = await connection.createChannel()
-    console.log('[RabbitMQ] ✅ Connected and Channel created')
-
-    return channel
-  } catch (error) {
-    console.error('[RabbitMQ] Failed to connect:', error.message)
-    setTimeout(getChannel, 10000)
+        channel = null
+        // Only reconnect on unexpected disconnects
+        if (!shuttingDown) {
+          setTimeout(getChannel, 10000) // reconnect after 10s
+        } else {
+          shuttingDown = false
+        }
+      })
+      channel = await connection.createChannel()
+      console.log('[RabbitMQ] ✅ Connected and Channel created')
+      return channel
+    } catch (error) {
+      console.error('[RabbitMQ] Failed to connect:', error.message)
+      console.log('[RabbitMQ] Reconnect in 10s...')
+      await new Promise((resolve) => setTimeout(resolve, 10000))
+    }
   }
+  return channel
 }
 
 export async function closeRabbit() {
